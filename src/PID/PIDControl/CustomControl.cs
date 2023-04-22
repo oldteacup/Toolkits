554e0ce9f854656e18d09f993573c5d329065142
lenovo <lenovo@lenovo.com>
2023-04-22 12:49:21 +0800

    pid 调参程序

---
+++ src/PID/PIDControl/CustomControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIDControl
+{
+    public class CustomControl : IControl
+    {
+        public double OutputValue { get; private set; } = 0;
+
+        public double ActualValue { get; private set; } = 0;
+
+        public double TargetValue { get; private set; } = 10;
+
+        public void Output(double output)
+        {
+            ActualValue = output;
+        }
+
+        public void Reset()
+        {
+            OutputValue = 0;
+            ActualValue = 0;
+            TargetValue = 10;
+        }
+    }
+}
---
+++ src/PID/PIDControl/IControl.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIDControl
+{
+    /// <summary>
+    /// 控制接口， ui通过控制接口实现控制的实现
+    /// </summary>
+    public interface IControl
+    {
+        /// <summary>
+        /// 当前输出值
+        /// </summary>
+        public double OutputValue { get; }
+
+        /// <summary>
+        /// 返回值
+        /// </summary>
+        public double ActualValue { get; }
+
+        /// <summary>
+        /// 目标值
+        /// </summary>
+        public double TargetValue { get; }
+
+        /// <summary>
+        /// 输出
+        /// </summary>
+        public void Output(double output);
+
+        /// <summary>
+        /// 重设
+        /// </summary>
+        public void Reset();
+    }
+}
